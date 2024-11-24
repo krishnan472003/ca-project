fba723ca3f7d773b3b71c6de3a371cbd9b7df503
krishnan <krishnansubramanian47@gmail.com>
2024-11-24 22:16:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// app.js
 
-function App() {
+import { useAuth } from "react-oidc-context";
+
+export default function App() {
+  // const auth = useAuth();
+
+  // const signoutRedireOut = () => {
+  //   const clientId = "5d2uqi9k49jcajrtfhoafn0iov"; 
+  //   const logoutUri = "<logout uri>";
+  //   const cognitoDomain = "<user pool domain>";
+  //   window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  // };
+
+  // if (auth.isLoading) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (auth.error) {
+  //   return <div>Encountering error... {auth.error.message}</div>;
+  // }
+
+  // if (auth.isAuthenticated) {
+  //   return (
+  //     <div>
+  //       <pre> Hello: {auth.user?.profile.email} </pre>
+  //       <pre> ID Token: {auth.user?.id_token} </pre>
+  //       <pre> Access Token: {auth.user?.access_token} </pre>
+  //       <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+        
+  //       <button onClick={() => auth.removeUser()}>Sign out</button>
+  //     </div>
+  //   );
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <>adsas</>
+      {/* <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signoutRedireOut()}>Sign out</button> */}
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { AuthProvider } from "react-oidc-context";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_zS0b7ZkYE",
+  client_id: "5d2uqi9k49jcajrtfhoafn0iov",
+  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+  response_type: "code",
+  scope: "phone openid email",
+};
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* <AuthProvider {...cognitoAuthConfig}> */}
+      <App />
+    {/* </AuthProvider> */}
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
